e0bcfdc14eccea9092eb8b91dbe1e1a259f36849
Sam Mead <samjamead@gmail.com>
2022-03-05 22:36:02 +0000

    you should use version control

---
+++ _posts/2022-02-12-jerseynutrition.markdown
@@ -0,0 +1,10 @@
+---
+layout:         post
+title:          "Jersey Nutrition"
+date:           2022-02-12 21:46:59 +0000
+img-feature:    jerseynutrition/jnFeatured.jpeg
+img-website:    jersea/jerseaScreenshot.png
+img-detail:     [jersea/jerseaDetail.png, jersea/jerseaDetail.png]
+project-intro:  "After partnering with British charity Blue Marine Foundation, Jersea needed an online home for all the         information hosted in their Fish of The Month campaign."
+project-stack:  "This project kicked off with a copywriting brief. The Jersea project uses Squarespace to give their team flexbility in the future. Once the copy was ticked off, we designed the web layout, and built a bespoke Squarespace theme to suit."
+---
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,111 @@
+
 jQuery(document).ready(function($){
 
-  var book = $(".book");
-  var controls = $(".controls");
-  var currentFace = '';
-
-  function spinBook() {
-    var activeFace = $("input[name='spin-book']:checked").val();
-    console.log(activeFace);
-    var showFace = 'show-' + activeFace;
-    if (currentFace) {
-      book.removeClass(currentFace);
-    }
-    book.addClass(showFace);
-    currentFace = showFace
+  function slideTo(el) {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $("#" + el.id).offset().top
+    }, 750);
   }
 
-  // Set initial view
-  spinBook();
+  $(document).on('keydown',function(e) {
+    switch(e.which) {
+      case 68:
+        window.location.href = "/#studioIntro";
+        break;
+      case 71:
+        slideTo(contact);
+        break;
+      case 72:
+        window.location.href = "/";
+        break;
+      case 78:
+        var nextSection = this;
+        console.log(nextSection);
+        break;
+      case 80:
+        window.location.href = "/process";
+        break;
+      case 83:
+        window.location.href = "/about";
+        break;
+      case 84:
+        $([document.documentElement, document.body]).animate({
+          scrollTop: 0
+        }, 750);
+        break;
+      case 87:
+        window.location.href = "/work";
+        break;
+      default:
+        // No thing
+        break;
+    }
+  });
 
-  controls.on('change', spinBook);
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://public.ecologi.com/users/samuelmead/trees",
+    "method": "GET",
+    "headers": {
+      "Content-Type": "application/json"
+    }
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    $(".treespan").html(response.total);
+  });
+
+  // All book spinning shiz
+  // var book = $(".book");
+  // var controls = $(".controls");
+  // var currentFace = '';
+  //
+  // function spinBook(cover) {
+  //   // var activeFace = $("input[name='spin-book']:checked").val();
+  //   var activeFace = cover;
+  //   var showFace = 'show-' + activeFace;
+  //   if (currentFace) {
+  //     book.removeClass(currentFace);
+  //   }
+  //   book.addClass(showFace);
+  //   currentFace = showFace
+  // }
+  //
+  // // Set initial view
+  // spinBook('spine');
+  //
+  // // controls.on('change', spinBook);
+  //
+  // var intersectionOptions = {
+  //   root: null,  // use the viewport
+  //   rootMargin: '0px',
+  //   threshold: 1.0
+  // }
+  //
+  // function intersectionCallback(entries, observer) {
+  //   entries.forEach(entry => {
+  //     if (entry.target.id == "transition1" && entry.intersectionRatio >= 0.9) {
+  //       spinBook('front');
+  //     } else if (entry.target.id == "transition2" && entry.intersectionRatio >= 0.9) {
+  //       spinBook('back');
+  //     } else {
+  //       spinBook('spine');
+  //     }
+  //   });
+  // }
+  //
+  // var observer = new IntersectionObserver(intersectionCallback, intersectionOptions);
+  //
+  // var target1 = document.querySelector('#transition1');
+  // var target2 = document.querySelector('#transition2');
+  //
+  // observer.observe(target1);
+  // observer.observe(target2);
+  //
+  // $.get( "https://public.ecologi.com/users/samuelmead/impact", function(data) {
+  //   console.log(data);
+  // });
 
 });
